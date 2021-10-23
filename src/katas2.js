4b4e4830d14ff47e05c5d5cbdbcbdd2edc7c19bd
lucasmori <91674466+LucasTakeoMori@users.noreply.github.com>
2021-10-23 01:15:09 -0300

    correçao kkkk

--- src/katas2.js
+++ src/katas2.js
@@ -1,41 +1,51 @@
 //função add
 function add(num1, num2) {
-    let result = num1 + num2
-
-    return result
+    return num1 + num2
 }
-console.assert(add(num1, num2))
+console.log(add(2,4 ))
 
 //---------------------------
 //função multiply
 function multiply(num1, num2) {
-    let result = num1 * num2
-
+    let result = 0
+    for (let contador = 0; contador < num1; contador++) {
+        result = add(num2,result)
+    }
     return result
 }
 
-console.log(multiply(num1, num2))
+console.log(multiply(2, 8))
 
 //---------------------------
 //função power
 function power(num1, num2) {
-    let result = num1 ** num2
-
+ let result = 1
+ for(let contador = 0; contador < num2; contador++)
+    {
+    result = multiply(num1,result)
+    }
     return result
 }
 
-console.log(power(num1, num2))
+console.log(power(2, 8))
 //---------------------------
 //função factorial
-function factorial(num)
-{
-    let numero = num
-    let resultado = numero
-    for (var contador = 1; contador < numero; contador++) {
-        resultado *= contador;
+function factorial(num) {
+    let result = 1
+    for (let contador = num ; contador >= 1; contador--) {
+        result = multiply(contador,result)
     }
-    return resultado
+    return result
 }
 
-console.log(factorial(num));
+console.log(factorial(4))
 //---------------------------
+//função fibonacci
+function fibonacci(num) {
+
+
+
+
+
+    
+}
